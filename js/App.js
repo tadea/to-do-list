e42121fdf7e14f367b8ba58ce4e86b94e5739ac3
Tadea <tsimunoviczg@gmail.com>
2020-03-15 11:51:31 -0400

    select query elements

---
+++ js/App.js
@@ -0,0 +1,5 @@
+//select elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
